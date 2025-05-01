e1c00839ce240932b6f6b795f40dc917f9bbd92e
Richaactualpixel <richa.thakkar@actualpixel.in>
2025-05-01 16:44:49 +0530

    Add test to return sum for two comma-separated numbers

--- string_calculator_test.rb
+++ string_calculator_test.rb
@@ -2,16 +2,21 @@
 require 'minitest/autorun'
 require_relative 'string_calculator'
 
+#Test cases for string calculator
 class StringCalculatorTest < Minitest::Test
   def setup
     @calculator = StringCalculator.new
   end
 
   def test_add_empty_string
-    assert_equal 0, @calculator.add("")
+    assert_equal 0, @calculator.add('')
   end
 
   def test_add_single_number
-    assert_equal 5, @calculator.add("5")
+    assert_equal 5, @calculator.add('5')
+  end
+
+  def test_add__two_numbers
+    assert_equal 6, @calculator.add('1,5')
   end
 end
