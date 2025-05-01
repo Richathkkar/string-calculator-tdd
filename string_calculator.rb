0736716dea99f8612f65f784d10133c3ad1004ae
Richaactualpixel <richa.thakkar@actualpixel.in>
2025-05-01 22:07:35 +0530

    Add private method and refactor the code

--- string_calculator.rb
+++ string_calculator.rb
@@ -2,27 +2,37 @@ class StringCalculator
 	def add(input)
 		return 0 if input.empty?
 		
-		# Handle custom delimiter at the beginning of the string
-		if input.start_with?("//")
-		  delimiter_section, input = input.split("\n", 2)
+		# Process the delimiters (either default or custom)
+		current_delimiters = process_delimiters(input)
+		
+		# Remove the custom delimiter declaration from the input string
+	    input = input.gsub(%r{//[^\n]+\n}, '')
+	 
+	    # Replace newlines with the first delimiter found in the input
+	    input.gsub!("\n", current_delimiters.first)
+	 
+	    # Split using the delimiters and convert them to integers
+	    numbers = input.split(Regexp.union(current_delimiters)).map(&:to_i)
 
-		  if delimiter_section.match?(/\[.*\]/)
-		    # Handle delimiter(s) of any length in square brackets
-		    delimiters = delimiter_section.scan(/\[(.*?)\]/).flatten
-		    delimiters.each do |delim|
-		      input = input.gsub(delim, ",")
-		    end
-		  else
-		    delimiter = delimiter_section[2]
-		    input = input.gsub(delimiter, ",")
-		  end
-		end
-
-	    input = input.gsub("\n", ",")
-		numbers = input.split(",").map(&:to_i)
+	    # Check for negative numbers and raise an error if any are found
 		negatives = numbers.select { |n| n < 0 }
 		raise "negative numbers not allowed #{negatives.join(', ')}" if negatives.any?
 
 	    numbers.select { |number| number <= 1000 }.sum # ignore numbers > 1000
+	end
+
+	private
+
+	def process_delimiters(input)
+     	if input.start_with?('//')
+	       # If custom delimiters are specified, check if there are multiple delimiters enclosed in square brackets
+	       if input.match?(%r{//\[(.*?)\]\n})
+	         input.scan(%r{\[(.*?)\]}).flatten # Extract all delimiters
+	       else
+	         [input.match(%r{//(.)\n})[1]] # Extract a single character delimiter if it's just one character
+	       end
+     	else
+       		[','] # Default delimiter
+     	end
 	end
 end
